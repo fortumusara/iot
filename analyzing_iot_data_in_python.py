ac09bbe8b221a8e6e189d85047e10d37f1388c7d
fortumusara <41570299+fortumusara@users.noreply.github.com>
2025-03-13 15:06:09 +0200

    Update analyzing_iot_data_in_python.py

--- analyzing_iot_data_in_python.py
+++ analyzing_iot_data_in_python.py
@@ -14,3 +14,61 @@ df_temp = pd.DataFrame(data_temp)
 
 print(df_temp.head())
 ----------------------------------------------------------------------
+# Import pandas
+import pandas as pd
+
+# Load URL to DataFrame
+df_temp = pd.read_json(URL)
+
+# Print first 5 rows
+print(df_temp.head())
+
+# Print datatypes
+print(df_temp.dtypes)
+----------------------------------------------------------------------
+df.to_json("abc.json", orient="records")
+----------------------------------------------------------------------
+# Import pandas
+import pandas as pd
+
+# Load URL to DataFrame
+df_temp = pd.read_json(URL)
+
+# Save DataFrame as json
+df_temp.to_json("temperature.json", orient="records")
+
+# Save DataFrame as csv without index
+df_temp.to_csv("temperature.csv", index=False)
+----------------------------------------------------------------------
+import pandas as pd
+
+# Read file
+df_env = pd.read_csv("environmental.csv", parse_dates=["timestamp"])
+
+# Print head
+print(df_env.head())
+
+# Print DataFrame info
+print(df_env.info())
+
+------------------------------------------------------------------------
+import pandas as pd
+
+# Read file
+df_env = pd.read_json("environmental.json")
+
+# Print head
+print(df_env.head())
+
+# Print DataFrame info
+print(df_env.info())
+------------------------------------------------------------------------
+import pandas as pd
+
+# Read file from json
+df_env = pd.read_json("environmental.json")
+
+# Print summary statistics
+print(df_env.describe())
+-----------------------------------------------------------------------
+
